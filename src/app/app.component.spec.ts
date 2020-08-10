5d2095cb716d278f1c08a56212266d5005207f02
Carlos Astorga <c.astorgavaras@hotmail.com>
2020-08-09 20:12:31 -0400

    Primer commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'superList'`, () => {
+  it(`should have as title 'list-super'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('superList');
+    expect(app.title).toEqual('list-super');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('superList app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('list-super app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl, AbstractControl } from '@angular/forms';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,110 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'superList';
+
+  productForm: FormGroup;
+
+  porcentaje: number = 0;
+
+  estaActualizando: boolean = false;
+  indice: number;
+
+  listaSuper: Producto[] = [];
+
+  get nombre(): AbstractControl {return this.productForm.get('nombre')}
+  get cantidad(): AbstractControl {return this.productForm.get('cantidad')}
+
+  constructor(private fb: FormBuilder){
+    const _lista = JSON.parse(localStorage.getItem("lista"));
+    this.listaSuper =  !_lista ?  [] : _lista;
+    this.productForm = this.fb.group({
+      nombre: ['', [Validators.required]],
+      cantidad: ['', [Validators.required]],
+    });
+    
+   
+  }
+
+  agregarProducto(){
+    console.log(this.productForm);
+    if(this.productForm.valid){
+      console.log('Formualrio valido');
+      const product: Producto = {
+        nombre: this.nombre.value,
+        cantidad: this.cantidad.value,
+        listo: false
+      }
+      this.listaSuper.push(product);
+      this.nombre.setValue('');
+      this.cantidad.setValue('');
+      this.actualizarPorcentaje();
+      localStorage.setItem("lista", JSON.stringify(this.listaSuper));
+      
+    }else{
+      alert('Formulario invalido');
+    }
+  }
+
+  actualizarPorcentaje(){
+    const largoLista = this.listaSuper.length;
+    const listos = this.listaSuper.filter((prod) => {
+      return prod.listo == true;
+    });
+    const cantProductListos = listos.length;
+    const resultado = (cantProductListos * 100) / largoLista;
+    if(isNaN(resultado)){
+      this.porcentaje = 0;
+    }
+    else{
+      this.porcentaje = resultado;
+    }
+    
+  }
+
+  checkProducto(producto: Producto){
+    producto.listo = !producto.listo;
+    this.actualizarPorcentaje();
+  }
+
+  eliminarProducto(indice:number){
+    if(this.estaActualizando){
+      alert('No se puede eliminar mientras se esta actualizando...');
+    }
+    else{
+      this.listaSuper.splice(indice,1);
+      localStorage.setItem("lista", JSON.stringify(this.listaSuper));
+      this.actualizarPorcentaje();
+    }
+    
+  }
+
+  editarProducto(product, index){
+    this.indice = index;
+    this.nombre.setValue(product.nombre);
+    this.cantidad.setValue(product.cantidad);
+    this.estaActualizando = true;
+  }
+
+  actualizar(){
+    if(this.productForm.valid){
+    console.log(this.indice);
+    const producto = this.listaSuper[this.indice];
+    producto.nombre = this.nombre.value;
+    producto.cantidad = this.cantidad.value;
+    this.nombre.setValue("");
+    this.cantidad.setValue("");
+    this.estaActualizando = false;
+    localStorage.setItem("lista", JSON.stringify(this.listaSuper));
+    console.log(this.nombre.value);
+    }else{
+      alert('Debe rellenar los campos');
+    }
+  }
+
+}
+
+export interface Producto{
+  nombre: string;
+  cantidad: number;
+  listo: boolean;
 }
